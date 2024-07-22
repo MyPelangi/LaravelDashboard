259d60c6b3446b459654918068bdca5be2ff77d5
MyPelangi <plngmawarni@gmail.com>
2024-07-22 09:48:40 +0700

    commit_v3

--- resources/views/sesi/login.blade.php
+++ resources/views/sesi/login.blade.php
@@ -6,9 +6,9 @@
         <div class="row">
             <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
                 <div class="login-brand">
-                <h1>Toko Segar</h1>
+                <h1>Toko Baru</h1>
                 </div>
-                
+
                 <div class="card card-primary">
                     <div class="card-header"><h4>Login</h4></div>
 
